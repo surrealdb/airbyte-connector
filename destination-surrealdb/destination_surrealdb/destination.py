c3bc905c90eca528fc31aa14e2505405c794026c
Yuske Kuoka <yusuke.kuoka@surrealdb.com>
2025-05-09 10:30:30 +0000

    Integration test along with Destination V2 support

--- destination-surrealdb/destination_surrealdb/destination.py
+++ destination-surrealdb/destination_surrealdb/destination.py
@@ -15,6 +15,7 @@ from surrealdb import Surreal
 
 from airbyte_cdk.destinations import Destination
 from airbyte_cdk.models import AirbyteConnectionStatus, AirbyteMessage, ConfiguredAirbyteCatalog, DestinationSyncMode, Status, Type
+from airbyte_cdk.sql.constants import AB_EXTRACTED_AT_COLUMN, AB_INTERNAL_COLUMNS, AB_META_COLUMN, AB_RAW_ID_COLUMN
 
 logger = getLogger("airbyte")
 
@@ -24,6 +25,7 @@ CONFIG_SURREALDB_DATABASE = "surrealdb_database"
 CONFIG_SURREALDB_TOKEN = "surrealdb_token"
 CONFIG_SURREALDB_USERNAME = "surrealdb_username"
 CONFIG_SURREALDB_PASSWORD = "surrealdb_password"
+
 
 def normalize_url(url: str) -> str:
     """
@@ -82,7 +84,6 @@ class DestinationSurrealDB(Destination):
     def write(
         self, config: Mapping[str, Any], configured_catalog: ConfiguredAirbyteCatalog, input_messages: Iterable[AirbyteMessage]
     ) -> Iterable[AirbyteMessage]:
-
         """
         Reads the input stream of messages, config, and catalog to write data to the destination.
 
@@ -109,9 +110,19 @@ class DestinationSurrealDB(Destination):
         con.query(f"DEFINE DATABASE IF NOT EXISTS {database};")
         con.use(namespace, database)
 
+        # See https://docs.airbyte.com/release_notes/upgrading_to_destinations_v2#breakdown-of-breaking-changes
+        is_legacyv1 = False
+        if "airbyte_destinations_version" in config:
+            is_legacyv1 = config["airbyte_destinations_version"] == "v1"
+        
+        do_write_raw = False
+        if "airbyte_write_raw" in config:
+            do_write_raw = config["airbyte_write_raw"]
+
+        dest_table_definitions = {}
+
         for configured_stream in configured_catalog.streams:
-            name = configured_stream.stream.name
-            table_name = f"_airbyte_raw_{name}"
+            table_name = configured_stream.stream.name
             if configured_stream.destination_sync_mode == DestinationSyncMode.overwrite:
                 # delete the tables
                 logger.info("Removing table for overwrite: %s", table_name)
@@ -119,9 +130,43 @@ class DestinationSurrealDB(Destination):
 
             # create the table if needed
             con.query(f"DEFINE TABLE IF NOT EXISTS {table_name};")
-            con.query(f"DEFINE FIELD IF NOT EXISTS _airbyte_ab_id ON {table_name} TYPE string;")
-            con.query(f"DEFINE FIELD IF NOT EXISTS _airbyte_emitted_at ON {table_name} TYPE datetime;")
-            con.query(f"DEFINE FIELD OVERWRITE _airbyte_data ON {table_name} TYPE string;")
+
+            looks_raw = table_name.startswith("airbyte_raw_")
+            fields_to_types = {}
+            if is_legacyv1:
+                fields_to_types = {
+                    "_airbyte_ab_id": "string",
+                    "_airbyte_emitted_at": "datetime"
+                }
+                if looks_raw:
+                    fields_to_types["_airbyte_data"] = "string"
+            else:
+                fields_to_types = {
+                    "_airbyte_raw_id": "string",
+                    "_airbyte_extracted_at": "datetime",
+                }
+                if looks_raw:
+                    fields_to_types["_airbyte_data"] = "object"
+                    fields_to_types["_airbyte_loaded_at"] = "datetime"
+                else:
+                    fields_to_types["_airbyte_meta"] = "object"
+            
+            stream_fields = configured_stream.stream.json_schema["properties"].keys()
+            for field_name in stream_fields:
+                props = configured_stream.stream.json_schema["properties"][field_name]
+                tpe = props["type"]
+                fmt = props["format"] if "format" in props else None
+                if tpe == "string" and fmt == "date-time":
+                    fields_to_types[field_name] = "datetime"
+                elif tpe == "integer":
+                    fields_to_types[field_name] = "int"
+                else:
+                    fields_to_types[field_name] = tpe
+
+            for field_name, field_type in fields_to_types.items():
+                con.query(f"DEFINE FIELD OVERWRITE {field_name} ON {table_name} TYPE {field_type};")
+
+            dest_table_definitions[table_name] = fields_to_types
 
         buffer = defaultdict(lambda: defaultdict(list))
 
@@ -130,41 +175,78 @@ class DestinationSurrealDB(Destination):
                 # flush the buffer
                 for stream_name in buffer.keys():
                     logger.info("flushing buffer for state: %s", message)
-                    DestinationSurrealDB._flush_buffer(con=con, buffer=buffer, stream_name=stream_name)
+                    DestinationSurrealDB._flush_buffer(
+                        con=con, buffer=buffer, stream_name=stream_name)
 
                 buffer = defaultdict(lambda: defaultdict(list))
-                
+
                 yield message
             elif message.type == Type.RECORD:
                 data = message.record.data
                 stream_name = message.record.stream
                 if stream_name not in streams:
-                    logger.debug("Stream %s was not present in configured streams, skipping", stream_name)
+                    logger.debug(
+                        "Stream %s was not present in configured streams, skipping", stream_name)
                     continue
+                emitted_at = message.record.emitted_at
+                emitted_at = datetime.datetime.fromtimestamp(emitted_at / 1000, datetime.timezone.utc)
+                loaded_at = datetime.datetime.now(datetime.timezone.utc)
                 # add to buffer
-                buffer[stream_name]["_airbyte_ab_id"].append(str(uuid.uuid4()))
-                buffer[stream_name]["_airbyte_emitted_at"].append(datetime.datetime.now())
-                buffer[stream_name]["_airbyte_data"].append(json.dumps(data))
-
+                raw_id = str(uuid.uuid4())
+                if is_legacyv1:
+                    # OLD Raw Table Columns
+                    # See https://docs.airbyte.com/release_notes/upgrading_to_destinations_v2#breakdown-of-breaking-changes
+                    buffer[stream_name]["_airbyte_ab_id"].append(raw_id)
+                    buffer[stream_name]["_airbyte_emitted_at"].append(emitted_at)
+                    buffer[stream_name]["_airbyte_loaded_at"].append(loaded_at)
+                else:
+                    record_meta: dict[str, str] = {}
+                    buffer[stream_name][AB_RAW_ID_COLUMN].append(raw_id)
+                    buffer[stream_name][AB_EXTRACTED_AT_COLUMN].append(loaded_at)
+                    buffer[stream_name][AB_META_COLUMN].append(record_meta)
+                if do_write_raw or stream_name.startswith("airbyte_raw_"):
+                    if is_legacyv1:
+                        # OLD Raw Table Columns
+                        # See https://docs.airbyte.com/release_notes/upgrading_to_destinations_v2#breakdown-of-breaking-changes
+                        buffer[stream_name]["_airbyte_data"].append(json.dumps(data))
+                    else:
+                        buffer[stream_name]["_airbyte_data"].append(data)
+                else:
+                    for field_name in data.keys():
+                        raw_data = data[field_name]
+                        if field_name not in dest_table_definitions[stream_name]:
+                            logger.error("field %s not in dest_table_definitions[%s]", field_name, stream_name)
+                            continue
+                        field_type = dest_table_definitions[stream_name][field_name]
+                        if field_type == "datetime":
+                            # This supports the following cases:
+                            # - "2022-06-20T18:56:18" in case airbyte_type is "timestamp_without_timezone"
+                            raw_data = datetime.datetime.fromisoformat(raw_data)
+                        buffer[stream_name][field_name].append(raw_data)
             else:
-                logger.info("Message type %s not supported, skipping", message.type)
-                    
+                logger.info(
+                    "Message type %s not supported, skipping", message.type)
+
         # flush any remaining messages
         for stream_name in buffer.keys():
-            DestinationSurrealDB._flush_buffer(con=con, buffer=buffer, stream_name=stream_name)
+            DestinationSurrealDB._flush_buffer(
+                con=con, buffer=buffer, stream_name=stream_name)
 
     @staticmethod
     def _flush_buffer(*, con: Surreal, buffer: Dict[str, Dict[str, List[Any]]], stream_name: str):
-        table_name = f"_airbyte_raw_{stream_name}"
+        table_name = stream_name
         buf = buffer[stream_name]
-        buf_ids = buf["_airbyte_ab_id"]
-        buf_emitted_at = buf["_airbyte_emitted_at"]
-        buf_data = buf["_airbyte_data"]
-        for i, _id in enumerate(buf_ids):
-            emitted_at = buf_emitted_at[i]
-            data = buf_data[i]
-            con.upsert(f"{table_name}:{_id}", {"_airbyte_ab_id": _id, "_airbyte_emitted_at": emitted_at, "_airbyte_data": data})
-
+        field_names = buf.keys()
+        id_field = "_airbyte_ab_id" if "_airbyte_ab_id" in field_names else AB_RAW_ID_COLUMN
+        id_column = buf[id_field]
+        for i, _id in enumerate(id_column):
+            record = {}
+            for field_name in field_names:
+                record[field_name] = buf[field_name][i]
+            try:
+                con.upsert(f"{table_name}:{_id}", record)
+            except Exception as e:
+                logger.error("error upserting record %s: %s", record, e)
 
     def check(self, logger: logging.Logger, config: Mapping[str, Any]) -> AirbyteConnectionStatus:
         """
@@ -187,4 +269,3 @@ class DestinationSurrealDB(Destination):
             return AirbyteConnectionStatus(status=Status.SUCCEEDED)
         except Exception as e:
             return AirbyteConnectionStatus(status=Status.FAILED, message=f"An exception occurred: {repr(e)}")
-        
